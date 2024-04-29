86073bde45657113029ecbfac76b834f6bdec9d0
JuanMa <juanmcicchelli@gmail.com>
2024-04-29 08:39:04 -0300

    se realizaron cambios en el 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/juanmcicchelli.js
@@ -0,0 +1,24 @@
+//javascript.com
+
+//Esto es una linea de comentario
+
+/* Esto es una comentario
+en varias lineas
+*/
+
+//Variable
+
+let x = 10;
+
+//Constante
+
+const y =100;
+
+//Variables tipo primitivos
+let es_integer = 10;
+let es_float = 1.2;
+let es_string = "Esto es un string";
+let es_booleano = True;
+
+//Muestra la cadena
+console.log("Hola, Javascript");
